9519d70299335766f157ada8071592d1a2c55a40
Gabrielmong <84334533+Gabrielmong@users.noreply.github.com>
2022-03-23 14:27:15 -0600

    Initial Upload

---
+++ Proyecto/src/main/java/ProyDesWeb/Proyecto/ProyDesWebApplication.java
@@ -0,0 +1,13 @@
+package ProyDesWeb.Proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyDesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyDesWebApplication.class, args);
+	}
+
+}
---
+++ Proyecto/src/test/java/ProyDesWeb/Proyecto/ProyDesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyDesWeb.Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyDesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
