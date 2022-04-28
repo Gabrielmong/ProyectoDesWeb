47a2c230471880be3a6b21bee113baf1b104f3f9
Gabrielmong <84334533+Gabrielmong@users.noreply.github.com>
2022-04-27 22:42:17 -0600

    Update SecurityConfig.java

--- Proyecto/src/main/java/com/Proyecto/SecurityConfig.java
+++ Proyecto/src/main/java/com/Proyecto/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/editPost/**")
+                .antMatchers("/editPost/**", "/eliminarPost/**")
                     .hasRole("ADMIN")
                 .and()
                     .formLogin()
